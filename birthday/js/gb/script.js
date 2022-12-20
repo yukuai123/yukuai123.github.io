ae79f5bf4ba5127f06220401d3a1ea3394fa887e
zhiyuan.lin <zhiyuan.lin@shihengtech.com>
2022-12-20 19:50:04 +0800

    feat: b&j

---
+++ birthday/js/gb/script.js
@@ -0,0 +1,18 @@
+function show_date_time() {
+    window.setTimeout("show_date_time()", 1000);
+    BirthDay = new Date("10/06/1993 00:00:00"); //这个日期是可以修改的
+    today = new Date();
+    timeold = (today.getTime() - BirthDay.getTime());
+    sectimeold = timeold / 1000
+    secondsold = Math.floor(sectimeold);
+    msPerDay = 24 * 60 * 60 * 1000
+    e_daysold = timeold / msPerDay
+    daysold = Math.floor(e_daysold);
+    e_hrsold = (e_daysold - daysold) * 24;
+    hrsold = Math.floor(e_hrsold);
+    e_minsold = (e_hrsold - hrsold) * 60;
+    minsold = Math.floor((e_hrsold - hrsold) * 60);
+    seconds = Math.floor((e_minsold - minsold) * 60);
+    span_dt_dt.innerHTML = daysold + "天" + hrsold + "小时" + minsold + "分" + seconds + "秒";
+}
+show_date_time();
